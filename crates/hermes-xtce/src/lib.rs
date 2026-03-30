925d3dac45841654d27d1a8d3d4c5efe7112b759
Andrei Tumbar <andrei.tumbar@jpl.nasa.gov>
2026-03-29 22:22:06 -0700

    Remove useless wrappers

--- crates/hermes-xtce/src/lib.rs
+++ crates/hermes-xtce/src/lib.rs
@@ -1,21 +1,3 @@
 #[allow(dead_code)]
 mod schema;
 pub use schema::xtce::*;
-
-// Workaround for quick-xml enum deserialization limitation
-// The generated RestrictionCriteriaType enum can't be deserialized when wrapped
-// in a <RestrictionCriteria> element. This wrapper struct fixes that.
-#[derive(Debug, serde::Deserialize, serde::Serialize)]
-pub struct RestrictionCriteriaWrapper {
-    #[serde(rename = "$value")]
-    pub inner: RestrictionCriteriaType,
-}
-
-// Re-export BaseContainer with the fixed restriction_criteria field
-#[derive(Debug, serde::Deserialize, serde::Serialize)]
-pub struct BaseContainerFixed {
-    #[serde(rename = "@containerRef")]
-    pub container_ref: NameReferenceWithPathType,
-    #[serde(default, rename = "RestrictionCriteria")]
-    pub restriction_criteria: ::core::option::Option<RestrictionCriteriaWrapper>,
-}
