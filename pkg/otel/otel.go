17a826aa5f734f6263eb7fe785685a7e69602d81
Yousef Khan <theyousefkhan@gmail.com>
2026-07-07 10:43:57 -0700

    go stubs + schema.jsoN

--- pkg/otel/otel.go
+++ pkg/otel/otel.go
@@ -1,1 +1,52 @@
 package otel
+
+import (
+	"context"
+
+	_ "embed"
+
+	"github.com/nasa/hermes/pkg/host"
+)
+
+var (
+	_ host.ProfileProvider[Params] = (*otelProvider)(nil)
+)
+
+//go:embed schema.json
+var schema string
+
+type Params struct {
+	Endpoint    string `json:"endpoint"`
+	ServiceName string `json:"serviceName"`
+	Events      bool   `json:"events"`
+	Telemetry   bool   `json:"telemetry"`
+}
+
+type otelProvider struct{}
+
+func (o *otelProvider) Default() Params {
+	return Params{
+		Endpoint:    "localhost:4317",
+		ServiceName: "hermes",
+		Events:      true,
+		Telemetry:   false,
+	}
+}
+
+func (o *otelProvider) Start(
+	ctx context.Context,
+	settings Params,
+	session host.ConnectSession,
+) error {
+	return nil
+}
+
+func Init() error {
+	_, err := host.RegisterProfileProviderWithUiSchema(
+		"OpenTelemetry",
+		&otelProvider{},
+		schema,
+		`{"ui:order": ["endpoint", "serviceName", "events", "telemetry"]}`,
+	)
+	return err
+}
